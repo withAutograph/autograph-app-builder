1b80b8880570c9e8d51c9b873bfa13e7fbb3857b
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-09-09 19:20:04 -0400

    test: follow app builder story move

--- .storybook/visual/app-builder.visual.spec.ts
+++ .storybook/visual/app-builder.visual.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test, type Page } from "playwright/test";
 
 const authenticatedStory =
-  "/iframe.html?id=create-app-page--default&viewMode=story";
+  "/iframe.html?id=create-app-flow-page--default&viewMode=story";
 const anonymousStory =
   "/iframe.html?id=create-app-flow-anonymous-entry--empty&viewMode=story";
 
